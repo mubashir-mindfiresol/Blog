604009bd69a4e953d92a5ed83652ebf09e006f21
MUBHASHIRAWIN10\mubhashira <sayedmubashira@mindfiresolutions.com>
2021-03-15 19:27:43 +0530

    NavBar and Signup
    Used Angular Material to create NavBar and SignUp. Also added Homepage, 404 Page and Logout Component. Added Routes too

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import {HomepageComponent} from './components/homepage/homepage.component';
+import { LogoutComponent } from './components/logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"/homepage", pathMatch:"full"},
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'logout', component: LogoutComponent},
+  {path:"**", component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatNativeDateModule} from '@angular/material/core'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SignupComponent } from './components/signup/signup.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { LogoutComponent } from './components/logout/logout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    MainNavComponent,
+    HomepageComponent,
+    NotfoundComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatSidenavModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
