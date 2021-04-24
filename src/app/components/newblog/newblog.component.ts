6c6853331622cc09d7621638676e1a07c1aa09e9
MUBHASHIRAWIN10\mubhashira <sayedmubashira@mindfiresolutions.com>
2021-04-24 20:02:24 +0530

    Some more CSS and logical changes in nav-bar

--- src/app/components/newblog/newblog.component.ts
+++ src/app/components/newblog/newblog.component.ts
@@ -52,20 +52,54 @@ export class NewblogComponent implements OnInit {
   }, 1500);
     this.fileInfos = this.uploadService.getFiles();
 
-    ClassicEditor
-    .create( document.querySelector( '#editor' ), {
-        toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
-        heading: {
-            options: [
-                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-                { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
-            ]
-        }
-    } )
-    .catch( error => {
-        console.log( error );
-    } );
+    // ClassicEditor
+    // .create( document.querySelector( '#editor' ), {
+    //     toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+    //     heading: {
+    //         options: [
+    //             { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+    //             { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+    //             { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+    //         ]
+    //     }
+    // } )
+    // .catch( error => {
+    //     console.log( error );
+    // } );
+}
+
+editor = ClassicEditor;
+data: any = ``;
+config = {
+  toolbar: [
+    'undo',
+    'redo',
+    '|',
+    'heading',
+    '|',
+    'bold',
+    'italic',
+    '|',
+    'link',
+    'mediaEmbed',
+    '|',
+    'bulletedList',
+    'numberedList',
+    '|',
+    'indent',
+    'outdent',
+    '|',
+    'insertTable',
+    'blockQuote',
+  ],
+  language: 'id',
+  image: {
+    toolbar: [
+      'imageTextAlternative',
+      'imageStyle:full',
+      'imageStyle:side'
+    ]
+  },
 }
 
   onSubmit() {
